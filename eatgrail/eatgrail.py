c9122083476f5a3a59e395eaaf91108ed66f543e
Nicholas Perry <ape.inago@gmail.com>
2018-08-17 14:53:42 -0400

    Now captures in_reply_to ids

--- eatgrail/eatgrail.py
+++ eatgrail/eatgrail.py
@@ -93,12 +93,12 @@ def parse_twitter_export_for_ids(EXPORT_LOCATION):
     print("    Found %s Total Tweets in Export" % tweet_count)
 
     print("  Begining extract.")
-    total_tweet_count, total_retweet_count, total_quotetweet_count = \
+    total_tweet_count, total_retweet_count, total_quotetweet_count, total_replyto_count = \
         extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION, is_zip_archive)
-    total_tweet_ids = total_tweet_count + total_retweet_count + total_quotetweet_count
+    total_tweet_ids = total_tweet_count + total_retweet_count + total_quotetweet_count + total_replyto_count
 
-    print("  Extracted %s tweets, %s retweets, %s quotetweets. Totalling %s tweets!" %
-          (total_tweet_count, total_retweet_count, total_quotetweet_count, total_tweet_ids))
+    print("  Extracted %s tweets, %s retweets, %s quotetweets, %s reply_tos. Totalling %s tweets!" %
+          (total_tweet_count, total_retweet_count, total_quotetweet_count, total_replyto_count, total_tweet_ids))
 
 
 def test_if_zip_archive(EXPORT_LOCATION):
@@ -140,7 +140,7 @@ def load_tweet_export_index(EXPORT_LOCATION, is_zip_archive):
                 index_filename = "data/js/tweet_index.js"
                 index_str = tweet_archive.read(index_filename).decode('utf-8')
                 index_str = re.sub(r'var tweet_index =', '', index_str)
-                print("    Successfully loaded \"%s\" from Zip Archive" % index_filename)
+                print("    loaded \"%s\" from Zip Archive" % index_filename)
                 return json.loads(index_str)
 
         except Exception as e:
@@ -156,7 +156,7 @@ def load_tweet_export_index(EXPORT_LOCATION, is_zip_archive):
             with open(index_filename) as index_file:
                 index_str = index_file.read()
                 index_str = re.sub(r'var tweet_index =', '', index_str)
-                print("    Successfully loaded \"%s\"" % index_filename)
+                print("    loaded \"%s\"" % index_filename)
                 return json.loads(index_str)
 
         except Exception as e:
@@ -175,20 +175,28 @@ def is_retweet(tweet):
 
 # grabs all the urls that link to twitter statuses and extracts IDs
 def extract_quoted_tweet_links(tweet):
-    urls = tweet['retweeted_status']['entities']['urls'] if is_retweet(tweet) \
-        else tweet['entities']['urls']
-
     quote_tweet_ids = list()
+    try:
 
-    for entry in urls:
-        parsed_url = urlparse(entry['expanded_url'])
-        if parsed_url.netloc.endswith("twitter.com"):
-            if "/status/" in parsed_url.path:
-                match = re.search("status/(\\d*)$", parsed_url.path)
-                if match is not None:
-                    quote_tweet_ids.append(match.group(1))
+        TWEET_ID_PATTERN = re.compile(r"status/(\d*)$")
 
-    return quote_tweet_ids
+        if is_retweet(tweet):
+            tweet = tweet['retweeted_status']
+
+        urls = tweet['entities']['urls']
+
+        for entry in urls:
+            parsed_url = urlparse(entry['expanded_url'])
+            if parsed_url.netloc.endswith("twitter.com"):
+                if "/status/" in parsed_url.path:
+                    match = TWEET_ID_PATTERN.search(parsed_url.path)
+                    if match is not None:
+                        quote_tweet_ids.append(match.group(1))
+
+        return quote_tweet_ids
+    except KeyError:
+        print("keyerror")
+        return quote_tweet_ids
 
 
 # empties out a file of existing data
@@ -203,17 +211,20 @@ def extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION, is_zip_ar
     total_tweet_count = 0
     total_retweet_count = 0
     total_quotetweet_count = 0
+    total_replyto_count = 0
 
     tweet_id_filename = "backup_export_tweet_ids.txt"
     retweet_id_filename = "backup_export_retweet_ids.txt"
     retweeted_id_filename = "backup_export_retweeted_ids.txt"
     quotetweet_id_filename = "backup_export_quotetweet_ids.txt"
+    replyto_id_filename = "backup_export_replyto_ids.txt"
 
     # clear out existing tweets to start from fresh (avoids duplicates if ran multiple times)
     clear_file(tweet_id_filename)
     clear_file(retweet_id_filename)
     clear_file(retweeted_id_filename)
     clear_file(quotetweet_id_filename)
+    clear_file(replyto_id_filename)
 
     # Loop 1: Go through all the months
     # ---------------------------------
@@ -222,6 +233,7 @@ def extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION, is_zip_ar
     retweet_ids_file = open(retweet_id_filename, 'a')
     retweeted_ids_file = open(retweeted_id_filename, 'a')
     quotetweet_ids_file = open(quotetweet_id_filename, 'a')
+    replyto_ids_file = open(replyto_id_filename, 'a')
 
     try:
         for date in tweets_by_month:
@@ -229,11 +241,10 @@ def extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION, is_zip_ar
                 year_str = '%04d' % date['year']
                 month_str = '%02d' % date['month']
 
-                print("    Parsing \"data/js/tweets/%s_%s.js\" " % (year_str, month_str), end='')
-
                 tweet_count = 0
                 retweet_count = 0
                 quotetweet_count = 0
+                replyto_count = 0
 
                 # Loop 2: Go through all the tweets in a month
                 # --------------------------------------------
@@ -251,7 +262,7 @@ def extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION, is_zip_ar
                             first_data_line = re.match(r'Grailbird.data.tweets_(.*) =', data_str).group(0)
                             data_str = re.sub(first_data_line, '', data_str)
                             data = json.loads(data_str)
-                            print("    Successfully loaded \"%s\" from Zip Archive" % zip_data_filename)
+                            #print("    Successfully loaded \"%s\" from Zip Archive" % zip_data_filename)
 
                     except Exception as e:
                         print("Could not open the Archive Zip!")
@@ -270,7 +281,7 @@ def extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION, is_zip_ar
                             first_data_line = re.match(r'Grailbird.data.tweets_(.*) =', data_str).group(0)
                             data_str = re.sub(first_data_line, '', data_str)
                             data = json.loads(data_str)
-                            print("    Successfully loaded \"%s\" from data Archive" % data_filename)
+                            #print("    Successfully loaded \"%s\" from data Archive" % data_filename)
 
                     except Exception as e:
                         print("Could not open the data file!")
@@ -283,43 +294,28 @@ def extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION, is_zip_ar
                 # handle each tweet
                 for tweet in data:
 
-                    media = tweet['retweeted_status']['entities']['media'] if is_retweet(tweet) \
-                        else tweet['entities']['media']
-
-                    tweet_id = str(tweet['id']) + "\n"
-
-                    retweeted_id = ""
-                    if is_retweet(tweet):
-                        retweeted_id = str(tweet['retweeted_status']['id']) + "\n"
-
                     # capture all retweet tweet IDs
                     if is_retweet(tweet):
-                        retweet_ids_file.write(tweet_id)
-                        retweeted_ids_file.write(retweeted_id)
-                        retweet_count += 1
+                        retweet_count += capture_retweet(tweet, retweet_ids_file, retweeted_ids_file)
 
                     # capture all non-retweet tweet IDs
                     else:
-                        tweet_ids_file.write(tweet_id)
-                        tweet_count += 1
+                        tweet_count += capture_tweet(tweet, tweet_ids_file)
 
-                    # c capture all embedded links to statuses (these are quote tweets!)
-                    for quotetweet_id in extract_quoted_tweet_links(tweet):
-                        quotetweet_id = str(quotetweet_id) + "\n"
-                        quotetweet_ids_file.write(quotetweet_id)
-                        quotetweet_count += 1
+                    # capture all embedded links to statuses (these are quote tweets!)
+                    quotetweet_count += capture_quotetweet(tweet, quotetweet_ids_file)
 
-                    if media:
-                        # Rewrite tweet date to be used in the filename prefix
-                        # (only first 19 characters + replace colons with dots)
-                        date = re.sub(r':', '.', tweet['created_at'][:19])
+                    # capture replyto to recreate context
+                    replyto_count += capture_replyto(tweet, replyto_ids_file)
 
                 # End loop 2 (tweets in a month)
 
-                print("    Parsed %s tweets, %s retweets, and %s quotetweets" % (tweet_count, retweet_count, quotetweet_count))
+                print("    Parsed %s tweets, %s retweets, %s quotetweets, and %s reply_tos, from \"data/js/tweets/%s_%s.js\"" %
+                      (tweet_count, retweet_count, quotetweet_count, replyto_count, year_str, month_str))
                 total_tweet_count += tweet_count
                 total_retweet_count += retweet_count
                 total_quotetweet_count += quotetweet_count
+                total_replyto_count += replyto_count
 
             # Nicer support for Ctrl-C
             except KeyboardInterrupt:
@@ -331,9 +327,60 @@ def extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION, is_zip_ar
         retweet_ids_file.close()
         retweeted_ids_file.close()
         quotetweet_ids_file.close()
+        replyto_ids_file.close()
 
     # End loop 1 (all the months)
-    return total_tweet_count, total_retweet_count, total_quotetweet_count
+    return total_tweet_count, total_retweet_count, total_quotetweet_count, total_replyto_count
+
+
+def capture_tweet(tweet, OUTPUT_FILE):
+    try:
+        if is_retweet(tweet):
+            return 0
+        else:
+            field = tweet['id']
+            OUTPUT_FILE.write(str(field) + "\n")
+            return 1
+    except KeyError:
+        return 0
+
+
+def capture_retweet(tweet, OUTPUT_FILE_RETWEETS, OUTPUT_FILE_RETWEETED_TWEETS):
+    try:
+        field = tweet['id']
+        if is_retweet(tweet):
+            OUTPUT_FILE_RETWEETS.write(str(field) + "\n")
+            capture_retweeted(tweet, OUTPUT_FILE_RETWEETED_TWEETS)
+            return 1
+        return 0
+    except KeyError:
+        return 0
+
+
+def capture_retweeted(tweet, OUTPUT_FILE):
+    try:
+        field = tweet['retweeted_status']['id']
+        OUTPUT_FILE.write(str(field) + "\n")
+        return 1
+    except KeyError:
+        return 0
+
+
+def capture_quotetweet(tweet, OUTPUT_FILE):
+    count = 0
+    for quotetweet_id in extract_quoted_tweet_links(tweet):
+        OUTPUT_FILE.write(str(quotetweet_id) + "\n")
+        count += 1
+    return count
+
+
+def capture_replyto(tweet, OUTPUT_FILE):
+    try:
+        field = tweet['in_reply_to_status_id']
+        OUTPUT_FILE.write(str(field) + "\n")
+        return 1
+    except KeyError:
+        return 0
 
 
 #
