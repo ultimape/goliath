ec41a91b5638abaa52acbfb50672e2dc254d7a7f
Nicholas Perry <ape.inago@gmail.com>
2018-08-17 10:16:56 -0400

    Extracting tweet ids directly from Zip Archive

--- eatgrail/eatgrail.py
+++ eatgrail/eatgrail.py
@@ -38,6 +38,7 @@ means. For more information, please refer to <http://unlicense.org/>
 import argparse
 import json
 import os
+import zipfile
 import re
 import sys
 import time
@@ -49,7 +50,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='A birdsite spider; For backing up your external brain')
     parser.add_argument('-extract', '--extract-backup',
                         type=str, dest='TWITTER_EXPORT_LOCATION',
-                        help="the location of the twitter export to parse for tweet IDs, as a directory")
+                        help="the location of the twitter export to parse for tweet IDs, as a directory, or a zip file")
     return parser.parse_args()
 
 
@@ -66,8 +67,22 @@ def parse_twitter_export_for_ids(EXPORT_LOCATION):
 
     EXPORT_LOCATION = EXPORT_LOCATION.strip().rstrip("/")
 
+    # some sanity checks for the export location
+    is_zip_archive = test_if_zip_archive(EXPORT_LOCATION)
+    is_corrupt = test_if_zip_archive_corrupt(EXPORT_LOCATION)
+
+    if is_zip_archive and is_corrupt:
+        print("Could not open the Archive Zip!")
+        print("    (location:\"%s\")" % EXPORT_LOCATION)
+        print("  It appears corrupt.")
+        print("")
+        sys.exit(-1)
+
+    if is_zip_archive:
+        print("  Archive is a Zip File.")
+
     print("  Attempting to load tweet export index.")
-    tweets_by_month = load_tweet_export_index(EXPORT_LOCATION)
+    tweets_by_month = load_tweet_export_index(EXPORT_LOCATION, is_zip_archive)
 
     month_count = len(tweets_by_month)
     print("    Found %s Months in Export" % month_count)
@@ -79,31 +94,78 @@ def parse_twitter_export_for_ids(EXPORT_LOCATION):
 
     print("  Begining extract.")
     total_tweet_count, total_retweet_count, total_quotetweet_count = \
-        extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION)
+        extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION, is_zip_archive)
     total_tweet_ids = total_tweet_count + total_retweet_count + total_quotetweet_count
 
     print("  Extracted %s tweets, %s retweets, %s quotetweets. Totalling %s tweets!" %
           (total_tweet_count, total_retweet_count, total_quotetweet_count, total_tweet_ids))
 
 
-# loads the twitter export index into a json compatible format for later digestion
-def load_tweet_export_index(EXPORT_LOCATION):
+def test_if_zip_archive(EXPORT_LOCATION):
+    if os.path.exists(EXPORT_LOCATION):
+        if os.path.isfile(EXPORT_LOCATION):
+            if zipfile.is_zipfile(EXPORT_LOCATION):
+                #print("  Archive selected is a Zip file.")
+                return True
+    return False
 
-    index_filename = EXPORT_LOCATION + "/data/js/tweet_index.js"
+
+def test_if_zip_archive_corrupt(EXPORT_LOCATION):
     try:
-        with open(index_filename) as index_file:
-            index_str = index_file.read()
-            index_str = re.sub(r'var tweet_index =', '', index_str)
-            print("    Successfully loaded \"%s\"" % index_filename)
-            return json.loads(index_str)
+        archive_file = zipfile.ZipFile(EXPORT_LOCATION)
 
-    except:
-        print("Could not open the data file!")
-        print("    (location:\"%s\")" % index_filename)
-        print("Please run this script from your tweet archive directory")
-        print("(the one with index.html file).")
-        print("")
-        sys.exit(-1)
+        corrupt_file = archive_file.testzip()
+        if corrupt_file is not None:
+            #print("  Corrupted file detected in Zip Archive! %s" % corrupt_file)
+            pass
+        else:
+            #print("  Zip Archive seems sound.")
+            return False
+    except Exception as e:
+        #print("  Archive was unable to be opened as a Zip. (Exception: %s)" % str(e))
+        return False
+
+    return True
+
+
+# loads the twitter export index into a json compatible format for later digestion
+def load_tweet_export_index(EXPORT_LOCATION, is_zip_archive):
+
+    # TODO: Handling Zips ought to be abstracted to avoid the duplication
+
+    if is_zip_archive:
+        try:
+            with zipfile.ZipFile(EXPORT_LOCATION) as tweet_archive:
+
+                index_filename = "data/js/tweet_index.js"
+                index_str = tweet_archive.read(index_filename).decode('utf-8')
+                index_str = re.sub(r'var tweet_index =', '', index_str)
+                print("    Successfully loaded \"%s\" from Zip Archive" % index_filename)
+                return json.loads(index_str)
+
+        except Exception as e:
+            print("Could not open the Archive Zip!")
+            print("    (location:\"%s\")" % EXPORT_LOCATION)
+            print("  (Exception: %s)" % str(e))
+            print("")
+            sys.exit(-1)
+
+    else:
+        index_filename = EXPORT_LOCATION + "/data/js/tweet_index.js"
+        try:
+            with open(index_filename) as index_file:
+                index_str = index_file.read()
+                index_str = re.sub(r'var tweet_index =', '', index_str)
+                print("    Successfully loaded \"%s\"" % index_filename)
+                return json.loads(index_str)
+
+        except Exception as e:
+            print("Could not open the data from the archive!")
+            print("    (location:\"%s\")" % index_filename)
+            print("Please ensure the location exists, and modify the -export parameter as nessisary")
+            print("  (Exception: %s)" % str(e))
+            print("")
+            sys.exit(-1)
 
 
 # checks if the twitter export tweet is a retweet
@@ -137,7 +199,7 @@ def clear_file(FILE_NAME):
 
 
 # goes through and loads each of the tweet files in the twitter export index and harvest the IDs
-def extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION):
+def extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION, is_zip_archive):
     total_tweet_count = 0
     total_retweet_count = 0
     total_quotetweet_count = 0
@@ -166,8 +228,8 @@ def extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION):
             try:
                 year_str = '%04d' % date['year']
                 month_str = '%02d' % date['month']
-                data_filename = EXPORT_LOCATION + '/data/js/tweets/%s_%s.js' % (year_str, month_str)
-                print("    Parsing %s " % data_filename, end='')
+
+                print("    Parsing \"data/js/tweets/%s_%s.js\" " % (year_str, month_str), end='')
 
                 tweet_count = 0
                 retweet_count = 0
@@ -176,14 +238,49 @@ def extract_tweets_from_tweet_export(tweets_by_month, EXPORT_LOCATION):
                 # Loop 2: Go through all the tweets in a month
                 # --------------------------------------------
 
-                with open(data_filename) as data_file:
-                    data_str = data_file.read()
-                    # Remove the assignment to a variable that breaks JSON parsing,
-                    # but save for later since we have to recreate the file
-                    first_data_line = re.match(r'Grailbird.data.tweets_(.*) =', data_str).group(0)
-                    data_str = re.sub(first_data_line, '', data_str)
-                    data = json.loads(data_str)
+                # Load the tweets from either the zip file, or the extracted archive
+                # TODO: Handling Zips ought to be abstracted to avoid the duplication
 
+                data = {}  # Data is initialized as empty
+                if is_zip_archive:
+                    try:
+                        with zipfile.ZipFile(EXPORT_LOCATION) as zip_data_file:
+
+                            zip_data_filename = 'data/js/tweets/%s_%s.js' % (year_str, month_str)
+                            data_str = zip_data_file.read(zip_data_filename).decode('utf-8')
+                            first_data_line = re.match(r'Grailbird.data.tweets_(.*) =', data_str).group(0)
+                            data_str = re.sub(first_data_line, '', data_str)
+                            data = json.loads(data_str)
+                            print("    Successfully loaded \"%s\" from Zip Archive" % zip_data_filename)
+
+                    except Exception as e:
+                        print("Could not open the Archive Zip!")
+                        print("    (location:\"%s\")" % zip_data_filename)
+                        print("  (Exception: %s)" % str(e))
+                        print("")
+                        sys.exit(-1)
+
+                else:
+                    try:
+                        data_filename = EXPORT_LOCATION + '/data/js/tweets/%s_%s.js' % (year_str, month_str)
+                        with open(data_filename) as data_file:
+                            data_str = data_file.read()
+                            # Remove the assignment to a variable that breaks JSON parsing,
+                            # but save for later since we have to recreate the file
+                            first_data_line = re.match(r'Grailbird.data.tweets_(.*) =', data_str).group(0)
+                            data_str = re.sub(first_data_line, '', data_str)
+                            data = json.loads(data_str)
+                            print("    Successfully loaded \"%s\" from data Archive" % data_filename)
+
+                    except Exception as e:
+                        print("Could not open the data file!")
+                        print("    (location:\"%s\")" % data_filename)
+                        print("Please ensure the location exists, and modify the -export parameter as nessisary")
+                        print("  (Exception: %s)" % str(e))
+                        print("")
+                        sys.exit(-1)
+
+                # handle each tweet
                 for tweet in data:
 
                     media = tweet['retweeted_status']['entities']['media'] if is_retweet(tweet) \
